ca190fa5af40d49c799f91a1ad2ec12f26b94da9
apham <admin@zez.duckdns.org>
2025-10-03 23:46:02 +0200

    njs

--- helper/scripts/install.sh
+++ helper/scripts/install.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(dirname $0)
 
+# JAVA
 mvn -f $SCRIPT_DIR/../../java-spring-boot/pom.xml install
 mvn -f $SCRIPT_DIR/../../java-plain/pom.xml install
 mvn -f $SCRIPT_DIR/../../java-spring-boot-camel/pom.xml install
@@ -32,5 +33,29 @@ sudo rm /usr/local/bin/jspring
 fi
 sudo cp $SCRIPT_DIR/jspring.sh /usr/local/bin/jspring
 sudo chmod 755 /usr/local/bin/jspring
+
+
+# zip template generator
+mkdir -p ~/.m2/repository/net/alainpham/applate/templategen/
+rm -r ~/.m2/repository/net/alainpham/applate/templategen/*
+cp -R  $SCRIPT_DIR/../templategen/* ~/.m2/repository/net/alainpham/applate/templategen/
+# Run npm install in the templategen directory
+npm install --prefix ~/.m2/repository/net/alainpham/applate/templategen/ 
+
+# nodejs
+
+if [ -f ~/.m2/repository/net/alainpham/applate/nodejs-express/nodejs-express.zip ]; then
+rm ~/.m2/repository/net/alainpham/applate/nodejs-express/nodejs-express.zip
+fi
+
+mkdir -p ~/.m2/repository/net/alainpham/applate/nodejs-express/
+rm -r ~/.m2/repository/net/alainpham/applate/nodejs-express/*
+zip -r ~/.m2/repository/net/alainpham/applate/nodejs-express/nodejs-express.zip $SCRIPT_DIR/../../nodejs-express -x '*node_modules*'
+
+if [ -f /usr/local/bin/njsexpress ]; then
+sudo rm /usr/local/bin/njsexpress
+fi
+sudo cp $SCRIPT_DIR/njsexpress.sh /usr/local/bin/njsexpress
+sudo chmod 755 /usr/local/bin/njsexpress
 
 echo "done"
---
+++ helper/scripts/njsexpress.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+node \
+    ~/.m2/repository/net/alainpham/applate/templategen/templategen.js \
+    ~/.m2/repository/net/alainpham/applate/nodejs-express/nodejs-express.zip \
+    nodejs-express \
+    $1
